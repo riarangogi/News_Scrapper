17d2bf899100aa53c82389eeea66ce75d0264911
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-07-10 15:26:07 -0500

    ok

--- Extract/main.py
+++ Extract/main.py
@@ -69,15 +69,13 @@ def _build_link(host, link):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-
-    news_site_choices = list(config()['news_sites'].keys())
-    parser.add_argument('news_site',
-                        help='The news site that you want to scrape',
-                        type=str,
-                        choices=news_site_choices)
-
-    args = parser.parse_args()
-    _news_scraper(args.news_site)
+	parser = argparse.ArgumentParser()
+	news_site_choices = list(config()['news_sites'].keys())
+	parser.add_argument('news_site',
+						help='The news site that you want to scrape',
+						type=str,
+						choices=news_site_choices)
+	args = parser.parse_args()
+	_news_scraper(args.news_site)
 
 
