2ee48872c0dad852d85378a8f42a02df646f8a21
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-07-10 10:46:27 -0500

    I created the ArticlePage class

--- Extract/news_page_objects.py
+++ Extract/news_page_objects.py
@@ -33,4 +33,22 @@ class HomePage(NewsPage):
 		return set(link['href'] for link in link_list)
 
 
+class ArticlePage(NewsPage):
+	def __init__(self, news_site_uid, url):
+		super().__init__(news_site_uid, url)
+
+	@property
+	def body(self):
+		result = self._select(self._queries['article_body'])
+		return result[0].text if len(result) else ''
+
+	@property
+	def title(self):
+		result = self._select(self._queries['article_title|'])
+		return result[0].text if len(result) else ''
+
+	@property
+	def url(self):
+		return self._url
+
 
