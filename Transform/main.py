505fbc65b43841e653a1b4a25bc90773b551d9ce
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-07-11 23:24:59 -0500

    I created the _extract_newspaper_uid function

--- Transform/main.py
+++ Transform/main.py
@@ -12,3 +12,10 @@ def _read_data(filename):
 	out_path = '../Data/Raw/'
 	logger.info('Reading file {}'.format(filename))
 	return pd.read_csv(out_path + filename)
+
+
+def _extract_newspaper_uid(filename):
+	logger.info('Extracting newspaper uid')
+	newspaper_uid = filename.split('_')[0]
+	logger.info('Newspaper uid detected: {}'.format(newspaper_uid))
+	return newspaper_uid
