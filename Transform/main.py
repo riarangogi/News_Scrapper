650e91e1c7f600e7fe2e00d714590051bb5a3263
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-07-13 19:19:34 -0500

    I added try and exctep to _missing_title function

--- Transform/main.py
+++ Transform/main.py
@@ -36,12 +36,15 @@ def _extract_host(df):
 def _fill_missing_titles(df):
 	logger.info('Filling missing titles')
 	missing_titles_mask = df['title'].isna()
-	missing_titles = (df[missing_titles_mask]['url']
+	try:
+		missing_titles = (df[missing_titles_mask]['url']
 						.str.extract(r'(?P<missing_titles>[^/]+)$')
 						.applymap(lambda title: title.split('-'))
 						.applymap(lambda title_word_list: ' '.join(title_word_list))
 						)
-	df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
+		df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
+	except:
+		next
 	return df
 
 
