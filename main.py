feb46227657e4d3fbe08199eef5eccb68c59105f
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-07-14 00:21:30 -0500

    I created _transform function

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import logging
 import subprocess
+import datetime
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 news_sites_uids = ['washingtonpost', 'elcolombiano', 'eltiempo', 'elespectador']
 
 def _extract():
@@ -11,5 +13,19 @@ def _extract():
 		subprocess.run(['python', 'main.py', news_site_uid], cwd='./Extract')
 
 
-if __name__ == '__main__':
+def _transform():
+	logger.info('Starting transform process')
+	now = datetime.datetime.now().strftime('%Y_%m_%d')
+	for news_site_uid in news_sites_uids:
+		dirty_data_filename = '{}_{}_articles.csv'.format(news_site_uid, now)
+		clean_data_filename = 'tidy_{}'.format(dirty_data_filename)
+		subprocess.run(['python', 'main.py', dirty_data_filename], cwd='./Transform')
+
+
+def main():
 	_extract()
+	_transform()
+
+
+if __name__ == '__main__':
+	main()
